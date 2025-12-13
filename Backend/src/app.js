2be0ea07f317b4ed42387dbe3337ba94c91d028c
unknown <hitesh.sharma176@gmail.com>
2025-12-13 19:02:24 +0530

    cors image issue fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  "/images",
+  express.static(path.join(process.cwd(), "images"), {
+    setHeaders: (res) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+    },
+  })
+);
 
 
 app.use(morgan('dev'));
